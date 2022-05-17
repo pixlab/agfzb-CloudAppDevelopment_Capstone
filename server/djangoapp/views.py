30d8bfef38de8ca69d56cd0ad12ab6b98cb7f5a1
roberto <robertoglaviano@gmail.com>
2022-05-17 16:17:44 +0000

    primo commit per esame final

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -16,14 +16,24 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
+def static(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/static.html', context)
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/about.html', context)
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+# Create an `about` view to render a static about page
+def contact(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
